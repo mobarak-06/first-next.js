7fa68c1320e64a0b6c056628f1ea48019723d24a
Mobarak Hossain <mobarakhossain121838@gmail.com>
2025-07-10 21:51:25 +0600

    nextjs basic auth added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+import LoginButton from "./components/LoginButton";
+import UserInfo from "./components/UserInfo";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <h2>home</h2>
+      <LoginButton/>
+      <UserInfo/>
     </div>
   );
 }
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,31 +1,34 @@
 import Link from "next/link";
 
 export const getPosts = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = res.json()
-    return data
-} 
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = res.json();
+  return data;
+};
 
+export const metadata = {
+  title: "All Posts | Learning Next js",
+  description: "Loading JSON Placeholder posts using Server Components",
+};
 
 export default async function posts() {
-    const posts = await getPosts();
-    console.log(posts);
+  const posts = await getPosts();
+  console.log(posts);
   return (
     <div className="grid grid-cols-4 gap-5 ">
-        {
-            posts.map(post => {
-                return(
-                  <div key={post.id} className="border-2 border-gray-300 p-4">
-                    
-                    <p>{post.title}</p>
-                    <p>{post.body}</p>
-                    <Link href={`/posts/${post.id}`}>
-                    <button className="p-2 bg-green-400 text-white font-semibold hover:bg-green-500 rounded-lg">Details</button>
-                    </Link>
-                  </div>  
-                )
-            })
-        }
+      {posts.map((post) => {
+        return (
+          <div key={post.id} className="border-2 border-gray-300 p-4">
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+            <Link href={`/posts/${post.id}`}>
+              <button className="p-2 bg-green-400 text-white font-semibold hover:bg-green-500 rounded-lg">
+                Details
+              </button>
+            </Link>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
